aa9974e5d3fa89c03a40c7b576ae39385eee70cb
Manpreet Bhatti <manpreet@bhatti.net>
2022-07-20 12:04:34 -0400

    Complete deleteDuplicates

---
+++ JavaScript/Easy/RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let curr = head;
+  while (curr && curr.next) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+};
