22a23817fabba0c5007281927bbf2de3ed69ea50
Manpreet Bhatti <manpreet@bhatti.net>
2022-07-24 17:33:23 -0400

    Completed inorderTraversal

---
+++ JavaScript/Easy/BinaryTreeInorderTreeTraversal.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let result = [];
+  if (root === null) {
+    return result;
+  }
+  result = result.concat(inorderTraversal(root.left));
+  result.push(root.val);
+  result = result.concat(inorderTraversal(root.right));
+  return result;
+};
