8b29d089827a8d78b16c1b506d68e31543b524e4
Manpreet Bhatti <manpreet@bhatti.net>
2022-10-31 14:01:30 -0400

    Finish Reverse Bits

---
+++ JavaScript/Easy/ReverseBits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function (n) {
+  let result = 0;
+  for (let i = 0; i < 32; i++) {
+    result = result * 2 + (n % 2);
+    n = Math.floor(n / 2);
+  }
+  return result;
+};
