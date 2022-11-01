9fb040eda0a2c778cbd3531791a7277396071555
Manpreet Bhatti <manpreet@bhatti.net>
2022-11-01 04:21:18 -0400

    Update Search Insert Position to use updated binary search

--- JavaScript/Easy/SearchInsertPosition.js
+++ JavaScript/Easy/SearchInsertPosition.js
@@ -9,20 +9,13 @@
 
 // Plan: Do a binary search on the array.
 var searchInsert = function (nums, target) {
-  let left = 0,
-    right = nums.length - 1,
-    mid = 0;
+  let left = 0;
+  let right = nums.length - 1;
   while (left <= right) {
-    mid = Math.floor((left + right) / 2);
-    // If the target is less than the mid, then the target should be in the
-    // left half of the array.
-    if (nums[mid] > target) right = mid - 1;
-    // If the target is greater than the mid, then the target should be in the
-    // right half of the array.
-    else if (nums[mid] < target) left = mid + 1;
-    else return mid;
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
   }
-  // If the target is not found, then return the index where the target should
-  // be inserted.
-  return nums[mid] > target ? mid : mid + 1;
+  return left;
 };
