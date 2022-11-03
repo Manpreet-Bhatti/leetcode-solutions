19839a87ccd10e9794a7a662a5e552b55887c158
Manpreet Bhatti <manpreet@bhatti.net>
2022-11-03 19:47:31 -0400

    Finished Valid Anagram

---
+++ JavaScript/Easy/ValidAnagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  let map = {};
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) {
+      map[s[i]]++;
+    } else {
+      map[s[i]] = 1;
+    }
+  }
+  for (let i = 0; i < t.length; i++) {
+    if (map[t[i]]) {
+      map[t[i]]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
