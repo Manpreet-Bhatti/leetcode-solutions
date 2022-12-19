2f09f929a675a5ac369dc48b9374d6c893004c7a
Manpreet Bhatti <manpreet@bhatti.net>
2022-12-18 21:13:21 -0500

    Finished Fibonacci Number

---
+++ JavaScript/Easy/FibonacciNumber.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+    if (n < 2) return n
+    let a = 0, b = 1
+    for (let i = 1; i < n; i++)
+        [a, b] = [b, a + b]
+    return b
+};
