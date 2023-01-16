352df44c32034d674e4e618a8513f75edb960a97
Manpreet Bhatti <manpreet@bhatti.net>
2023-01-15 22:45:12 -0500

    Finished Range Addition II

---
+++ JavaScript/Easy/RangeAdditionII.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+var maxCount = function (m, n, ops) {
+    for (var i = 0; i < ops.length; i++) {
+        m = Math.min(m, ops[i][0]);
+        n = Math.min(n, ops[i][1]);
+    }
+    return m * n;
+};
