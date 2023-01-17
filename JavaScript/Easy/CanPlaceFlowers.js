bc3a1352b283775199775ca7f34658f67a4d5c72
Manpreet Bhatti <manpreet@bhatti.net>
2023-01-17 15:21:02 -0500

    Finished Can Place Flowers

---
+++ JavaScript/Easy/CanPlaceFlowers.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    let count = 0;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+            flowerbed[i] = 1;
+            count++;
+        }
+    }
+    return count >= n;
+};
