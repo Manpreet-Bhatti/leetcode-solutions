010a865de8d44b194bd50e5606274ae01c5623f3
Manpreet Bhatti <manpreet@bhatti.net>
2023-01-25 00:15:29 -0500

    Update Robot Return to Origin

--- JavaScript/Easy/RobotReturnToOrigin.js
+++ JavaScript/Easy/RobotReturnToOrigin.js
@@ -5,21 +5,16 @@
 var judgeCircle = function (moves) {
     var x = 0;
     var y = 0;
+    var movements = {
+        "U": [0, 1],
+        "D": [0, -1],
+        "L": [-1, 0],
+        "R": [1, 0]
+    };
     for (var i = 0; i < moves.length; i++) {
-        switch (moves[i]) {
-            case "U":
-                y++;
-                break;
-            case "D":
-                y--;
-                break;
-            case "L":
-                x--;
-                break;
-            case "R":
-                x++;
-                break;
-        }
+        var move = movements[moves[i]];
+        x += move[0];
+        y += move[1];
     }
     return x === 0 && y === 0;
 };
