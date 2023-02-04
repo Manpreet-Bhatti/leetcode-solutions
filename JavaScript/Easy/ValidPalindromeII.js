f6ae1ab8c4105153481636e43844d683dd66cf07
Manpreet Bhatti <manpreet@bhatti.net>
2023-02-03 22:15:06 -0500

    Finished Valid Palindrome II

---
+++ JavaScript/Easy/ValidPalindromeII.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s, i, j) {
+    while (i < j) {
+        if (s[i] != s[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+var validPalindrome = function (s) {
+    var i = 0, j = s.length - 1;
+    while (i < j) {
+        if (s[i] != s[j]) {
+            return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+        }
+        i++;
+        j--;
+    }
+    return true;
+};
