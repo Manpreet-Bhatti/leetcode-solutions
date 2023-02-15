be5f524b46eeaad35b56958263d61122e6161744
Manpreet Bhatti <manpreet@bhatti.net>
2023-02-15 16:34:48 -0500

    Add second implementation to To Lower Case

--- JavaScript/Easy/ToLowerCase.js
+++ JavaScript/Easy/ToLowerCase.js
@@ -5,3 +5,26 @@
 var toLowerCase = function (s) {
     return s.toLowerCase();
 };
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+    const A = 'A'.charCodeAt(0);
+    const Z = 'Z'.charCodeAt(0);
+    const diff = 'a'.charCodeAt(0) - A;
+    let result = '';
+
+    for (let i = 0; i < s.length; i++) {
+        const code = s.charCodeAt(i);
+
+        if (code >= A && code <= Z) {
+            result += String.fromCharCode(code + diff);
+        } else {
+            result += s[i];
+        }
+    }
+
+    return result;
+};
