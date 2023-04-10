e842e7f1d859f0599c8e20672e8c84ad569a229c
Manpreet-Bhatti <manpreet@bhatti.net>
2023-04-10 17:12:34 -0400

    Update Valid Parantheses

--- JavaScript/Easy/ValidParentheses.js
+++ JavaScript/Easy/ValidParentheses.js
@@ -2,35 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-
 var isValid = function (s) {
   let stack = [];
+  const openers = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  };
+  const closers = new Set([")", "}", "]"]);
+
   for (let i = 0; i < s.length; i++) {
-    if (s[i] == "(" || s[i] == "[" || s[i] == "{") {
-      // Add to stack if there's an opening parenthesis
+    if (openers[s[i]]) {
       stack.push(s[i]);
-    }
-    // If no opening parentheses, return false
-    if (stack.length === 0) {
-      return false;
-    }
-    let match;
-    // If closing parenthesis, check if it matches the last opening parenthesis
-    switch (s[i]) {
-      case ")":
-        match = stack.pop();
-        if (match == "{" || match == "[") return false;
-        break;
-      case "}":
-        match = stack.pop();
-        if (match == "(" || match == "[") return false;
-        break;
-      case "]":
-        match = stack.pop();
-        if (match == "(" || match == "{") return false;
-        break;
+    } else if (closers.has(s[i])) {
+      if (s[i] !== openers[stack.pop()]) {
+        return false;
+      }
     }
   }
-  // Check to see if there are no parentheses
+
   return stack.length === 0;
 };
