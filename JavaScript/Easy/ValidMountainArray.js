1b0e27d685dde0add9970c8ab4ba76f60815a314
Manpreet-Bhatti <manpreet@bhatti.net>
2023-05-19 17:02:31 -0400

    Finished Valid Mountain Array

---
+++ JavaScript/Easy/ValidMountainArray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function (arr) {
+    const n = arr.length;
+    let i = 0;
+
+    while (i < n - 1 && arr[i] < arr[i + 1]) {
+        i++;
+    }
+
+    if (i === 0 || i === n - 1) {
+        return false;
+    }
+
+    while (i < n - 1 && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    return i === n - 1;
+};
