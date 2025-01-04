5ae6c46557e57579924c05a8bae3d410d101abab
Manpreet Bhatti <manpreet@bhatti.net>
2025-01-03 21:52:48 -0500

    Finish Divide an Array Into Subarrays With Minimum Cost I

---
+++ JavaScript/Easy/DivideAnArrayIntoSubarraysWithMinimumCostI.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumCost = function (nums) {
+  const firstNum = nums[0];
+
+  let smallest = Infinity;
+  let secondSmallest = Infinity;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < smallest) {
+      secondSmallest = smallest;
+      smallest = nums[i];
+    } else if (nums[i] < secondSmallest) {
+      secondSmallest = nums[i];
+    }
+  }
+
+  return firstNum + smallest + secondSmallest;
+};
+
+console.log(minimumCost([1, 2, 3]));
